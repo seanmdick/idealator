4abad6f05e5e06ad80808a2c0e93c982a2fdfded
Sean Dick <sean@seandick.net>
2011-01-16 12:05:46 -0800

    ajax voting via rails helpers

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,13 +1,16 @@
 class VotesController < ApplicationController
   def create
-    topic = Topic.find(params[:topic_id])
-    vote = topic.votes.build
+    @topic = Topic.find(params[:topic_id])
+    vote = @topic.votes.build
 
     if vote.save
      flash[:notice] = 'Vote was successfully created.'
     else
      flash[:notice] = 'Sorry we could not count your vote.'
     end
-    redirect_to(topics_path)
+    respond_to do |format|
+      format.html {redirect_to(topics_path)}
+      format.js   {}
+    end
   end
 end
