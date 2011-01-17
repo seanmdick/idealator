ddaa8903f5098ac6acd92fba9ac9df9905a0588f
Amy Lee <amy@seqmedia.com>
2011-01-16 18:40:15 -0800

    When the New Topic button is clicked make sure to clear out the old form contents.

--- public/javascripts/topic.js
+++ public/javascripts/topic.js
@@ -1,4 +1,14 @@
-$('.topic_link .new_topic').live("click", function(){$('.form_container').toggleClass("link form")});
+$('.topic_link .new_topic').live("click", function(){
+    // Visibly toggle the form.
+    $('.form_container').toggleClass("link form")
+    
+    // Clear out old contents.
+    $('.form_container input[type=text]').val('');
+    $('.form_container textarea').val('');
+    
+    // Focus on the title
+    $('.form_container #topic_title').focus();
+});
 $('.topic_form form').live('submit', function(e) {
   e.preventDefault(); 
   $('.form_container').toggleClass("link form");
