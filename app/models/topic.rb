e0918ee0a31ef07fc01b6c0a7a4687a5371ba215
Amy Lee <amy@seqmedia.com>
2011-01-17 06:35:10 -0800

    Using after-save callback on Vote to tell Topic to recount its votes.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,8 @@
 class Topic < ActiveRecord::Base
   has_many :votes
+  
+  def update_total_votes
+    self.total_votes = votes.length
+    self.save!
+  end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,10 @@ class Vote < ActiveRecord::Base
   # Touch updates the Topic when the vote is saved
   # (http://ryandaigle.com/articles/2009/4/20/what-s-new-in-edge-rails-touching)
   belongs_to :topic, :touch => true
+  
+  after_save :update_topic_total_votes
+  
+  def update_topic_total_votes
+    topic.update_total_votes
+  end
 end
