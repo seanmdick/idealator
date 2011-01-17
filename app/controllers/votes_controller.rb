b541df48db9ad7053716204eb16185ed13471a8c
Nathan Esquenazi <nesquena@gmail.com>
2011-01-16 19:07:49 -0800

    votes controller: cleans up the code

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,30 +1,33 @@
 class VotesController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
-    vote = @topic.votes.build
+    @vote = @topic.votes.build
 
-    if vote.save
-     flash.now[:notice] = 'Vote was successfully created.'
+    if @vote.save
+      flash.now[:notice] = 'Vote was successfully created.'
     else
-     flash.now[:error] = 'Sorry we could not count your vote.'
+      flash.now[:error] = 'Sorry we could not count your vote.'
     end
+
     respond_to do |format|
-      format.html {redirect_to(topics_path)}
-      format.js   {}
+      format.html { redirect_to(topics_path) }
+      format.js   # renders votes/create.js.erb
     end
   end
-  
+
   def destroy
     @topic = Topic.find(params[:topic_id])
-    
-    if @topic.votes.first.try(:destroy)
-     flash.now[:notice] = 'Vote was successfully Destroyed.'
+    @vote  = @topic.votes.first
+
+    if @vote && @vote.destroy
+      flash.now[:notice] = 'Vote was successfully retracted.'
     else
-     flash.now[:error] = 'Sorry we could not count your vote.'
+      flash.now[:error] = 'Sorry we could not count your vote.'
     end
+
     respond_to do |format|
-      format.html {redirect_to(topics_path)}
-      format.js   {}
+      format.html { redirect_to(topics_path) }
+      format.js   # renders votes/destroy.js.erb
     end
   end
 end
